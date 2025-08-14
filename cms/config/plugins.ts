fb5ca51d64b4abc554f9b19a5fb6d070efeb259f
CPompey1 <Cpompey10@gmail.com>
2025-08-14 16:16:12 -0400

    prod changes

--- cms/config/plugins.ts
+++ cms/config/plugins.ts
@@ -1,3 +1,9 @@
+import crypto from 'crypto';
+
 export default () => ({
-    
+  'users-permissions': {
+    config: {
+      jwtSecret: process.env.JWT_SECRET || crypto.randomBytes(16).toString('base64'),
+    },
+  },
 });
